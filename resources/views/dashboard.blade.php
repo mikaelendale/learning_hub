41ab42292a5146f8d909bc18d9e32e23c8d6910c
mikaelendale <149874638+mikaelendale@users.noreply.github.com>
2024-07-20 16:09:40 +0300

    cleared

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @if (Auth::user()->id == '27')
+                        <div class="flex flex-col">
+                            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                    <div class="  overflow-hidden sm:rounded-lg">
+                                        {{__('you have certificate')}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @else
+                        {{ __('welcome') }}
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="shortcut icon" href="https://lalodev.com/images/icon.png" type="image/x-icon">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
