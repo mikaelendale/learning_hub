d36c90017b2a3f1c6a2c1d31d93365f712fef147
mikaelendale <149874638+mikaelendale@users.noreply.github.com>
2024-08-20 12:33:29 +0300

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,23 @@ class DashboardController extends Controller
         $courses = Courses::all();
         $user = Auth::user();
         $enrolledStudents = Students::inRandomOrder()->limit(7)->get();
+        // Fetch the number of available courses from the courses table
+        $availableCourses = Courses::count(); // Total number of courses
 
-        return view('pages/index', compact('courses', 'enrolledStudents'));
+        // Fetch the number of students based on class_attended and level
+        $studentsByClassAndLevel = Students::select('class_attended', 'level')
+            ->groupBy('class_attended', 'level')
+            ->get()
+            ->mapWithKeys(function ($item) {
+                return [$item->class_attended . '-' . $item->level => $item->count()];
+            });
+
+        // Fetch the total number of enrolled students
+        $enrolledCourses = Students::whereHas('courses')->count();
+
+        // Fetch the total number of students
+        $totalStudents = Students::count(); 
+
+        return view('pages/index', compact('courses', 'enrolledStudents','availableCourses', 'studentsByClassAndLevel', 'enrolledCourses', 'totalStudents'));
     }
 }
