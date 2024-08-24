cd10ceb56fe0156f030d553644b64aa5768706b0
mikaelendale <149874638+mikaelendale@users.noreply.github.com>
2024-08-24 19:31:14 +0300

    Update start.blade.php

--- resources/views/pages/quiz/start.blade.php
+++ resources/views/pages/quiz/start.blade.php
@@ -43,32 +43,32 @@
              <div class="flex justify-center gap-2.5">
                  <div class="flex justify-center items-center min-h-screen">
                      <div class="card grow" style="width: 600px;" id="question_1">
-                         <div class="card-body px-12 py-10 lg:px-16 lg:py-12">
+                         <div class="card-body px-12 py-10 lg:px-16 lg:py-12" style="user-select: none;">
                              <div class="flex flex-col gap-6">
-                                 <h2 class="text-2xl font-semibold text-gray-900 text-start">
+                                 <h2 class="text-2xl font-semibold text-gray-900 text-start" style="user-select: none;">
                                      1. What is the purpose of our blog?
                                  </h2>
                                  <div class="grid grid-cols-1 gap-5 lg:gap-7.5">
                                      <div class="col-span-1">
-                                         <div class="flex flex-col items-start gap-4">
-                                             <label class="radio-group">
+                                         <div class="flex flex-col items-start gap-4" style="user-select: none;">
+                                             <label class="radio-group" style="user-select: none;">
                                                  <input class="radio" name="desktop_notification" type="radio"
                                                      value="1">
-                                                 <span class="radio-label radio-label-sm">
+                                                 <span class="radio-label radio-label-sm" style="user-select: none;">
                                                      All new messages (Recommended)
                                                  </span>
                                              </label>
-                                             <label class="radio-group">
+                                             <label class="radio-group" style="user-select: none;">
                                                  <input checked="" class="radio" name="desktop_notification"
                                                      type="radio" value="2">
-                                                 <span class="radio-label radio-label-sm">
+                                                 <span class="radio-label radio-label-sm" style="user-select: none;">
                                                      Direct @mentions
                                                  </span>
                                              </label>
-                                             <label class="radio-group">
+                                             <label class="radio-group" style="user-select: none;">
                                                  <input checked="" class="radio" name="desktop_notification"
                                                      type="radio" value="3">
-                                                 <span class="radio-label radio-label-sm">
+                                                 <span class="radio-label radio-label-sm" style="user-select: none;">
                                                      Disabled
                                                  </span>
                                              </label>
@@ -78,175 +78,41 @@
                              </div>
                          </div>
                      </div>
-                 </div>
-
-
-
-                 <div class="card grow hidden" id="question_2">
-                     <div class="card-header">
-                         <h3 class="card-title">
-                             Activity
-                         </h3>
-                         <div class="flex items-center gap-2">
-                             <label class="switch">
-                                 <span class="switch-label">
-                                     Auto refresh:
-                                     <span class="switch-on:hidden">
-                                         Off
-                                     </span>
-                                     <span class="hidden switch-on:inline">
-                                         On
-                                     </span>
-                                 </span>
-                                 <input checked="" name="check" type="checkbox" value="1" />
-                             </label>
-                         </div>
-                     </div>
-                     <div class="card-body">
-                         <div class="flex flex-col">
-                             <div class="flex items-start relative">
-                                 <div class="w-9 left-0 top-9 absolute bottom-0 translate-x-1/2 border-l border-l-gray-300">
-                                 </div>
-                                 <div
-                                     class="flex items-center justify-center shrink-0 rounded-full bg-gray-100 border border-gray-300 size-9 text-gray-600">
-                                     <i class="ki-filled ki-people text-base">
-                                     </i>
-                                 </div>
-                                 <div class="pl-2.5 mb-7 text-md grow">
-                                     <div class="flex flex-col">
-                                         <div class="text-sm font-medium text-gray-800">
-                                             Posted a new article
-                                             <a class="text-sm font-medium link"
-                                                 href="../public-profile/profiles/blogger.html">
-                                                 Top 10 Tech Trends
-                                             </a>
+                      <div class="card grow hidden" style="width: 600px;" id="question_2">
+                         <div class="card-body px-12 py-10 lg:px-16 lg:py-12" style="user-select: none;">
+                             <div class="flex flex-col gap-6">
+                                 <h2 class="text-2xl font-semibold text-gray-900 text-start" style="user-select: none;">
+                                     2. What is the purpose of our blog?
+                                 </h2>
+                                 <div class="grid grid-cols-1 gap-5 lg:gap-7.5">
+                                     <div class="col-span-1">
+                                         <div class="flex flex-col items-start gap-4" style="user-select: none;">
+                                             <label class="radio-group" style="user-select: none;">
+                                                 <input class="radio" name="" type="radio"
+                                                     value="1">
+                                                 <span class="radio-label radio-label-sm" style="user-select: none;">
+                                                     All new messages (Recommended)
+                                                 </span>
+                                             </label>
+                                             <label class="radio-group" style="user-select: none;">
+                                                 <input checked="" class="radio" name="desktop_notification"
+                                                     type="radio" value="2">
+                                                 <span class="radio-label radio-label-sm" style="user-select: none;">
+                                                     Direct @mentions
+                                                 </span>
+                                             </label>
+                                             <label class="radio-group" style="user-select: none;">
+                                                 <input checked="" class="radio" name="desktop_notification"
+                                                     type="radio" value="3">
+                                                 <span class="radio-label radio-label-sm" style="user-select: none;">
+                                                     Disabled
+                                                 </span>
+                                             </label>
                                          </div>
-                                         <span class="text-xs font-medium text-gray-500">
-                                             Today, 9:00 AM
-                                         </span>
-                                     </div>
-                                 </div>
-                             </div>
-                             <div class="flex items-start relative">
-                                 <div class="w-9 left-0 top-9 absolute bottom-0 translate-x-1/2 border-l border-l-gray-300">
-                                 </div>
-                                 <div
-                                     class="flex items-center justify-center shrink-0 rounded-full bg-gray-100 border border-gray-300 size-9 text-gray-600">
-                                     <i class="ki-filled ki-share text-base">
-                                     </i>
-                                 </div>
-                                 <div class="pl-2.5 mb-7 text-md grow">
-                                     <div class="flex flex-col">
-                                         <div class="text-sm font-medium text-gray-800">
-                                             I couldn't resist sharing a sneak peek of our
-                                             <a class="text-sm font-medium link"
-                                                 href="../public-profile/profiles/blogger.html">
-                                                 upcoming content
-                                             </a>
-                                         </div>
-                                         <span class="text-xs font-medium text-gray-500">
-                                             5 days ago, 4:07 PM
-                                         </span>
-                                     </div>
-                                 </div>
-                             </div>
-                             <div class="flex items-start relative">
-                                 <div class="w-9 left-0 top-9 absolute bottom-0 translate-x-1/2 border-l border-l-gray-300">
-                                 </div>
-                                 <div
-                                     class="flex items-center justify-center shrink-0 rounded-full bg-gray-100 border border-gray-300 size-9 text-gray-600">
-                                     <i class="ki-filled ki-coffee text-base">
-                                     </i>
-                                 </div>
-                                 <div class="pl-2.5 mb-7 text-md grow">
-                                     <div class="flex flex-col">
-                                         <div class="text-sm font-medium text-gray-800">
-                                             Reaching the milestone of
-                                             <a class="text-sm font-medium link"
-                                                 href="../public-profile/profiles/feeds.html">
-                                                 10,000 followers
-                                             </a>
-                                             on the blog was a dream come true
-                                         </div>
-                                         <span class="text-xs font-medium text-gray-500">
-                                             5 days ago, 4:07 PM
-                                         </span>
-                                     </div>
-                                 </div>
-                             </div>
-                             <div class="flex items-start relative">
-                                 <div class="w-9 left-0 top-9 absolute bottom-0 translate-x-1/2 border-l border-l-gray-300">
-                                 </div>
-                                 <div
-                                     class="flex items-center justify-center shrink-0 rounded-full bg-gray-100 border border-gray-300 size-9 text-gray-600">
-                                     <i class="ki-filled ki-design-1 text-base">
-                                     </i>
-                                 </div>
-                                 <div class="pl-2.5 mb-7 text-md grow">
-                                     <div class="flex flex-col">
-                                         <div class="text-sm font-medium text-gray-800">
-                                             Onboarded a talented designer to our creative team, adding valuable expertise
-                                             to upcoming projects.
-                                         </div>
-                                         <span class="text-xs font-medium text-gray-500">
-                                             2 weeks ago, 10:45 AM
-                                         </span>
-                                     </div>
-                                 </div>
-                             </div>
-                             <div class="flex items-start relative">
-                                 <div
-                                     class="w-9 left-0 top-9 absolute bottom-0 translate-x-1/2 border-l border-l-gray-300">
-                                 </div>
-                                 <div
-                                     class="flex items-center justify-center shrink-0 rounded-full bg-gray-100 border border-gray-300 size-9 text-gray-600">
-                                     <i class="ki-filled ki-like-shapes text-base">
-                                     </i>
-                                 </div>
-                                 <div class="pl-2.5 mb-7 text-md grow">
-                                     <div class="flex flex-col">
-                                         <div class="text-sm font-medium text-gray-800">
-                                             Hosted a virtual
-                                             <a class="text-sm font-medium link"
-                                                 href="../public-profile/profiles/creator.html">
-                                                 team-building event
-                                             </a>
-                                             , fostering collaboration and strengthening bonds among team members.
-                                         </div>
-                                         <span class="text-xs font-medium text-gray-500">
-                                             1 month ago, 13:56 PM
-                                         </span>
-                                     </div>
-                                 </div>
-                             </div>
-                             <div class="flex items-start relative">
-                                 <div
-                                     class="flex items-center justify-center shrink-0 rounded-full bg-gray-100 border border-gray-300 size-9 text-gray-600">
-                                     <i class="ki-filled ki-cup text-base">
-                                     </i>
-                                 </div>
-                                 <div class="pl-2.5 text-md grow">
-                                     <div class="flex flex-col">
-                                         <div class="text-sm font-medium text-gray-800">
-                                             We recently
-                                             <a class="text-sm font-medium link"
-                                                 href="../public-profile/profiles/nft.html">
-                                                 celebrated
-                                             </a>
-                                             the blog's 1-year anniversary
-                                         </div>
-                                         <span class="text-xs font-medium text-gray-500">
-                                             3 months ago, 4:07 PM
-                                         </span>
                                      </div>
                                  </div>
                              </div>
                          </div>
-                     </div>
-                     <div class="card-footer justify-center">
-                         <a class="btn btn-link" href="#">
-                             All-time Activity
-                         </a>
                      </div>
                  </div>
              </div>
